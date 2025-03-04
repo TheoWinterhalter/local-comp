68911144a5fe310c665c3d2d286abe224d2a1e29
Théo Winterhalter <theo.winterhalter@gmail.com>
2025-03-04 21:49:19 +0100

    Almost done with typing_einst

--- theories/BasicMetaTheory.v
+++ theories/BasicMetaTheory.v
@@ -682,8 +682,7 @@ Proof.
     + econstructor. 1,3: eassumption.
       intros M x E ξ' Ξ'' Θ R B hM hE hx.
       rewrite <- einst_eget. rewrite <- einst_einst.
-      (* TODO: Maybe a lemma to type those from hξ? *)
-      admit.
+      eauto.
     + rewrite einst_einst. reflexivity.
   - cbn. subst rξ. rewrite eget_ren.
     eapply meta_conv.
