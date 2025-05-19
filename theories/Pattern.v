93cee7d9c54d55b1ce6a56371b9ea417e5c9a8c7
Théo Winterhalter <theo.winterhalter@gmail.com>
2025-05-19 17:26:56 +0200

    Draft rho-like relation

--- theories/Pattern.v
+++ theories/Pattern.v
@@ -98,22 +98,22 @@ Section Red.
 
   (** Congruence rules **)
 
-  | red_Pi A B A' B' :
+  | pred_Pi A B A' B' :
       Γ ⊢ A ⇒ A' →
       Γ ,, A ⊢ B ⇒ B' →
       Γ ⊢ Pi A B ⇒ Pi A' B'
 
-  | red_lam A t A' t' :
+  | pred_lam A t A' t' :
       Γ ⊢ A ⇒ A' →
       Γ ,, A ⊢ t ⇒ t' →
       Γ ⊢ lam A t ⇒ lam A' t'
 
-  | red_app u v u' v' :
+  | pred_app u v u' v' :
       Γ ⊢ u ⇒ u' →
       Γ ⊢ v ⇒ v' →
       Γ ⊢ app u v ⇒ app u' v'
 
-  | red_const c ξ ξ' :
+  | pred_const c ξ ξ' :
       Forall2 (Forall2 (pred Γ)) ξ ξ' →
       Γ ⊢ const c ξ ⇒ const c ξ'
 
@@ -210,8 +210,137 @@ Section Red.
     all: eauto.
   Qed.
 
+  (** ** Maximal reduct for parallel reduction *)
+
+  Definition is_lam t :=
+    match t with
+    | lam A t => true
+    | _ => false
+    end.
+
+  Reserved Notation "Γ ⊢ u ⇒ᵨ v"
+    (at level 80, u, v at next level).
+
+  Inductive pred_max Γ : term → term → Prop :=
+  | pred_max_beta A t t' u u' :
+      Γ ,, A ⊢ t ⇒ᵨ t' →
+      Γ ⊢ u ⇒ᵨ u' →
+      Γ ⊢ app (lam A t) u ⇒ᵨ t' <[ u' .. ]
+
+  | pred_max_unfold c ξ Ξ' A t ξ' t' :
+      Σ c = Some (Def Ξ' A t) →
+      inst_equations Σ Ξ Γ ξ Ξ' →
+      closed t = true →
+      ∙ ⊢ t ⇒ t' →
+      Forall2 (Forall2 (pred_max Γ)) ξ ξ' →
+      Γ ⊢ const c ξ ⇒ᵨ einst ξ' t'
+
+  | pred_max_rule E Ξ' Δ R M ξ' n rule σ σ' :
+      Σ E = Some (Ext Ξ' Δ R) →
+      ectx_get Ξ M = Some (E, ξ') →
+      nth_error R n = Some rule →
+      let δ := length Δ in
+      let lhs := rlhs M ξ' δ rule in
+      let rhs := rrhs M ξ' δ rule in
+      let k := length rule.(cr_env) in
+      scoped k lhs = true →
+      scoped k rhs = true →
+      (∀ m, Γ ⊢ σ m ⇒ᵨ σ' m) →
+      Γ ⊢ lhs <[ σ ] ⇒ᵨ rhs <[ σ' ]
+
+  (** Congruence rules **)
+
+  | pred_max_Pi A B A' B' :
+      Γ ⊢ A ⇒ᵨ A' →
+      Γ ,, A ⊢ B ⇒ᵨ B' →
+      Γ ⊢ Pi A B ⇒ᵨ Pi A' B'
+
+  | pred_max_lam A t A' t' :
+      Γ ⊢ A ⇒ᵨ A' →
+      Γ ,, A ⊢ t ⇒ᵨ t' →
+      Γ ⊢ lam A t ⇒ᵨ lam A' t'
+
+  | pred_max_app u v u' v' :
+      is_lam u = false →
+      Γ ⊢ u ⇒ᵨ u' →
+      Γ ⊢ v ⇒ᵨ v' →
+      Γ ⊢ app u v ⇒ᵨ app u' v'
+
+  | predmax_const c ξ ξ' :
+      Forall2 (Forall2 (pred_max Γ)) ξ ξ' →
+      Γ ⊢ const c ξ ⇒ᵨ const c ξ'
+
+  where "Γ ⊢ u ⇒ᵨ v" := (pred_max Γ u v).
+
+  Lemma triangle Γ t u :
+    Γ ⊢ t ⇒ u →
+    ∃ tᵨ, Γ ⊢ t ⇒ᵨ tᵨ ∧ Γ ⊢ u ⇒ tᵨ.
+  Proof.
+    induction 1 as [
+      ?????? ht iht hu ihu
+    | ???????????? iht ? ihξ
+    | ????????????????????? ih
+    | ?????? ihA ? ihB
+    | ?????? ihA ? iht
+    | ?????? ihu ? ihv
+    | ???? ih
+    ] using pred_ind_alt.
+    - destruct iht as [tr [ht1 ht2]], ihu as [ur [hu1 hu2]].
+      eexists. split.
+      + econstructor. all: eassumption.
+      + (* Need substitution lemma for pred *) admit.
+    - destruct iht as [tr [ht1 ht2]].
+      admit.
+    - (* The current ∀∃ is not enough to get a substitution
+        It might be easier with an implementation of matching.
+        We could also define a ρ function instead.
+
+        We will anyway need to define matching for the rule selection thing
+        when we get more interesting patterns.
+      *)
+      admit.
+    - destruct ihA as [Ar [hA1 hA2]], ihB as [Br [hB1 hB2]].
+      eexists. split.
+      + econstructor. all: eassumption.
+      + econstructor. all: eauto. admit.
+    - destruct ihA as [Ar [hA1 hA2]], iht as [tr [ht1 ht2]].
+      eexists. split.
+      + econstructor. all: eassumption.
+      + econstructor. all: eauto. admit.
+    - destruct ihu as [ur [hu1 hu2]], ihv as [vr [hv1 hv2]].
+      eexists. split.
+      + econstructor. all: eauto.
+        (* Maybe this approach is not the best in the end? *)
+        admit.
+      + econstructor. all: eassumption.
+    - admit.
+  Admitted.
+
+  Lemma pred_max_functional Γ t u v :
+    Γ ⊢ t ⇒ᵨ u →
+    Γ ⊢ t ⇒ᵨ v →
+    u = v.
+  Proof.
+  Admitted.
+
+  Lemma diamond Γ t u v :
+    Γ ⊢ t ⇒ u →
+    Γ ⊢ t ⇒ v →
+    ∃ w, Γ ⊢ u ⇒ w ∧ Γ ⊢ v ⇒ w.
+  Proof.
+    intros hu hv.
+    eapply triangle in hu as [w [hw ?]], hv as [? []].
+    eapply pred_max_functional in hw as e. 2: eassumption.
+    subst. exists w. intuition eauto.
+  Qed.
+
 End Red.
 
 Notation "Σ ;; Ξ | Γ ⊢ u ⇒ v" :=
   (pred Σ Ξ Γ u v)
   (at level 80, u, v at next level).
+
+(* Not really needed *)
+(* Notation "Σ ;; Ξ | Γ ⊢ u ⇒ᵨ v" :=
+  (pred_max Σ Ξ Γ u v)
+  (at level 80, u, v at next level). *)
